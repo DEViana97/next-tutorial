770a10f1b411b160b71ccfa93bfbfa9997fdfa53
Matheus Viana <mateusviana97@gmail.com>
2024-08-29 13:18:03 -0300

    Learning about authentication, middleware and how to use

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,10 @@
 'use server';
-
 import { z } from "zod"
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from '@/app/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -103,4 +104,24 @@ export async function deleteInvoice (id: string) {
   }
 
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    console.log("action: ", formData)
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
